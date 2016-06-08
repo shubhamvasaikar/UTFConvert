6869403611401f3adb11b80d8866840449734d4d
shubhamvasaikar <shubham.vasaikar7@gmail.com>
2016-06-08 13:51:47 +0530

    final code

--- convert.py
+++ convert.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
-utf16hex = eval(input("Enter the UTF-16 Hex code: "));
+utf16hex = eval(input("Enter the UTF-16 Hex code: (Format: 0xXXXX): "));
 
 print("Original UTF-16 hex code: " + hex(utf16hex))
 
 if(utf16hex >= 0x00) and (utf16hex <=0x007f):
 	utf8hex = utf16hex
+	print("\nBit representation of converted code: ");
+	print("Byte1 : " + bin(utf8hex));	
 	
 elif(utf16hex >= 0x0080) and (utf16hex <=0x07ff):
 	utf8byte1 = utf16hex >> 6
@@ -15,10 +17,11 @@ elif(utf16hex >= 0x0080) and (utf16hex <=0x07ff):
 	utf8byte2 = utf8byte2 + 0x80
 	
 	utf8byte1 = utf8byte1 << 8
-	
 	utf8hex = utf8byte1 + utf8byte2
+	utf8byte1 = utf8byte1 >> 8
+		
+	print("\nBit representation of converted code: ");
+	print("Byte1 : " + bin(utf8byte1));
+	print("Byte2 : " + bin(utf8byte2));	
 	
 print("\nHex code of UTF-16 converted to UTF-8: " + hex(utf8hex))
-print("Bit representation of converted code: ");
-print("Byte1 : " + bin(utf8byte1));
-print("Byte2 : " + bin(utf8byte2));
